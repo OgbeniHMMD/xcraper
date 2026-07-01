2ea4fc4c37e2d606d6cb31f6ae84fa925c47c641
Hammed A. Olajide <olajide.a.hammed@gmail.com>
2026-07-01 21:52:17 +0100

    chore: initialize node_modules directory with tailwindcss and project dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./dashboard/**/*.html",
+    "./dashboard/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'x-blue': '#1da1f2',
+        'x-green': '#17bf63',
+        'x-red': '#e0245e',
+      }
+    },
+  },
+  plugins: [],
+}
